742fa2dc9a24e10f93f3a106c4673748be067b71
Anuj Aggarwal <anuj.aggarwal63@gmail.com>
2018-04-08 22:24:29 +0530

    Make Structure for Auth Routes and Remove body-parser from package.json

---
+++ routes/auth.js
@@ -0,0 +1,21 @@
+// Create Express Router
+const route = require("express").Router();
+// Passport
+const Passport = require("passport");
+
+const { User } = require("./models");
+
+
+// POST Route for Signup
+
+
+
+// POST Route for Login
+
+
+
+// GET Route for Logout
+
+
+
+module.exports = route;
