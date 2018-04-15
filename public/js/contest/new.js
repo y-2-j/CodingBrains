2f91c409606042041d062091e157afae12141abc
Anuj Aggarwal <anuj.aggarwal63@gmail.com>
2018-04-15 18:14:07 +0530

    Show Flash Message on Invalid Forms and Errors in Creating Contest

--- public/js/contest/new.js
+++ public/js/contest/new.js
@@ -37,9 +37,7 @@ $(() => {
         const startTime = $startTimeInput.val();
         const duration = $durationInput.val();
 
-        if (name === "" || isNaN(duration))
-            return;
-        if (!moment(startTime).isValid() || moment(startTime).diff(Date.now(), "days") <= 0)
+        if (!validateForm(name, startTime, duration))
             return;
 
         // TODO: Validate for all Form Fields
@@ -52,6 +50,45 @@ $(() => {
          })
          .catch(err => {
              console.error(err.stack);
+             displayFlashMessage("Something went Wrong, Please try again!");
          });
     });
 });
+
+// Function to Display the Flash Message
+const displayFlashMessage = (flashText) => {
+    const $flashMessage = $("#flash-message");
+    $flashMessage.text(flashText);
+    $flashMessage.show();
+};
+
+// Function to Hide the Flash Message
+const hideFlash = () => $("#flash-message").hide();
+
+// Function to validate the Form Fields
+// and Display Flash Message if not valid
+// Returns true if Form Valid
+//         false if invalid
+const validateForm = (name, startTime, duration) => {
+    // Check for all Required Fields
+    if (name === "") {
+        displayFlashMessage("Name is Required!");
+        return false; 
+    }
+    if (!moment(startTime).isValid()) {
+        displayFlashMessage("Invalid Start Time!!");
+        return false;
+    }
+    if (duration === "" || isNaN(duration)) {
+        displayFlashMessage("Duration must be an Integer!!");
+        return false;
+    }
+    
+    // Check for Validity of Start Time
+    if (moment(startTime).diff(Date.now(), "days") <= 0) {
+        displayFlashMessage("Sorry, Contest Registration must be before 1 Day of Start of Contest!");
+        return false;
+    }
+
+    return true;
+};
